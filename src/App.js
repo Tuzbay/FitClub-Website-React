d5929b79528a29f13298155eaa9f50dbaaad062c
Tuzbay <t.uzbayyelce@gmail.com>
2022-08-22 17:38:22 +0300

    Programs Components Added!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Hero from "./Hero"
+import Programs from "./Programs"
 
 function App() {
   return (
     <div className="app">
-      
     <Hero />
-
+    <Programs />
 
     </div>
   );
