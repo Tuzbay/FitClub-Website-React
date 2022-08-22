4d6ee5f800e9a7d3e526a7e3b87670dd85201aa0
Tuzbay <t.uzbayyelce@gmail.com>
2022-08-22 23:01:18 +0300

    Reasons Component Added!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Hero from "./Hero"
 import Programs from "./Programs"
+import Reasons from "./Reasons"
 
 function App() {
   return (
     <div className="app">
     <Hero />
     <Programs />
-
+    <Reasons />
     </div>
   );
 }
