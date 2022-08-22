5485d4fef702bbd0ceab1e4938a1c0cc1ae2b8df
Tuzbay <t.uzbayyelce@gmail.com>
2022-08-22 12:08:01 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Hero from "./Hero"
+
+function App() {
+  return (
+    <div className="app">
+      
+    <Hero />
+
+    <Header />
+
+    </div>
+  );
+}
+
+export default App;
